99bf49e3426fa805df130b16e1385f9c88ec1e83
comaplatan-create <comaplatan@gmail.com>
2026-05-29 07:10:01 +0000

    Import global CSS before App to ensure styles load

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import {StrictMode} from 'react';
+import './index.css';
 import {createRoot} from 'react-dom/client';
 import App from './App';
-import './index.css';
 import { ThemeProvider } from './lib/ThemeContext';
 
 createRoot(document.getElementById('root')!).render(
